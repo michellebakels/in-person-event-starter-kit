5225e9c0f20af3dce264a1b3fe96dcbefa09e6e8
michellebakels <michelle.bakels@gmail.com>
2022-07-22 12:02:09 -0400

    force commit

--- components/schedule.tsx
+++ components/schedule.tsx
@@ -21,6 +21,7 @@ import TalkCard from './talk-card';
 
 function DayRow({ day }: { day: Day }) {
   // Group talks by the time block
+  console.log("force commit")
   const timeBlocks = day.schedule.reduce((allBlocks: any, talk) => {
     allBlocks[talk.start] = [...(allBlocks[talk.start] || []), talk];
     return allBlocks;
